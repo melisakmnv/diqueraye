331215653ad19eb820ec5f3bbcf70f2d8c1918fe
Ekaraja <melisakmnv@gmail.com>
2025-03-08 16:32:32 +0100

    fixed carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,70 +1,95 @@
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const sliders = document.querySelectorAll(".disque-slider");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const sliders = document.querySelectorAll(".disque-slider");
 
-    if (!sliders.length) return;
+//     if (!sliders.length) return;
 
-    const list = [];
+//     const list = [];
 
-    sliders.forEach((element) => {
-        const [slider, prevEl, nextEl, pagination] = [
-            element.querySelector(".swiper"),
-            element.querySelector(".slider-nav__item_prev"),
-            element.querySelector(".slider-nav__item_next"),
-        ];
+//     sliders.forEach((element) => {
+//         const [slider, prevEl, nextEl, pagination] = [
+//             element.querySelector(".swiper"),
+//             element.querySelector(".slider-nav__item_prev"),
+//             element.querySelector(".slider-nav__item_next"),
+//         ];
 
-        console.log("first")
+//         console.log("first")
 
-        list.push(
-            new Swiper(slider, {
-                effect: "coverflow",
-                slidesPerView: 3,
-                spaceBetween: 40,
-                loop: true,
-                speed: 800,
-                observer: true,
-                watchOverflow: true,
-                watchSlidesProgress: true,
-                centeredSlides: true,
-                initialSlide: 1,
-                navigation: { nextEl, prevEl, disabledClass: "disabled" },
-                coverflowEffect: {
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 50,
-                    modifier: 1,
-                    slideShadows: false,
-                    scale: 0.95,
-                },
-                breakpoints: {
-                    768: { spaceBetween: 40 }
-                }
-            })
-        );
-    });
+//         list.push(
+//             new Swiper(slider, {
+//                 effect: "coverflow",
+//                 slidesPerView: 3,
+//                 spaceBetween: 40,
+//                 loop: true,
+//                 speed: 800,
+//                 observer: true,
+//                 watchOverflow: true,
+//                 watchSlidesProgress: true,
+//                 centeredSlides: true,
+//                 initialSlide: 1,
+//                 navigation: { nextEl, prevEl, disabledClass: "disabled" },
+//                 coverflowEffect: {
+//                     rotate: 0,
+//                     stretch: 0,
+//                     depth: 10,
+//                     modifier: 1,
+//                     slideShadows: false,
+//                     scale: 0.85,
+//                 },
+//                 breakpoints: {
+//                     1280: { 
+//                         speed: 800,
+//                         spaceBetween: 0, 
+//                         slidesPerView: 3,
+//                         coverflowEffect: {
+//                             rotate: 0,
+//                             stretch: 80,
+//                             depth: 50,
+//                             scale: 0.5,
+//                         },
+
+//                      }
+
+//                 }
+//             })
+//         );
+//     });
+
+// });
 
 
-    // // ========================================== //
-    // // ============ ROTATE MINI LOGO ============ //
-    // // ========================================== //
+/*=============== SWIPER JS ===============*/
+var swiper = new Swiper(".card__content", {
+    effect: "coverflow",
+    // grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    initialSlide: 2,
+    loop: true,
+    speed: 500,
+    spaceBetween: 64,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 50,
+        modifier: 1,
+        slideShadows: false,
+        scale: 0.85,
+    },
 
-    // const DURATION = 3000;
+    navigation: {
+        nextEl: ".slider-nav__item_next",
+        prevEl: ".slider-nav__item_prev",
+    },
+    breakpoints: {
+        1980: {
+            speed: 800,
+            spaceBetween: 0,
+            slidesPerView: 3,
 
-    // const logo = document.getElementById("logo");
 
-    // function spinLogo() {
-    //     logo.classList.add("animate-spin-slow");
+        }
 
-    //     // Stop spinning after 1s to restart cleanly
-    //     setTimeout(() => {
-    //         logo.classList.remove("animate-spin-slow");
-    //     }, 1000);
-    // }
-
-    // // Start spinning immediately
-    // spinLogo();
-
-    // // Repeat the spin every 5 seconds
-    // setInterval(spinLogo, DURATION);
+    }
 });
