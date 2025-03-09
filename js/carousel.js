b4a16f956d3efacc8efec1ebf67c59fec7e388fc
Ekaraja <melisakmnv@gmail.com>
2025-03-09 11:43:24 +0100

    added images and links on slides

--- js/carousel.js
+++ js/carousel.js
@@ -1,64 +1,5 @@
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const sliders = document.querySelectorAll(".disque-slider");
-
-//     if (!sliders.length) return;
-
-//     const list = [];
-
-//     sliders.forEach((element) => {
-//         const [slider, prevEl, nextEl, pagination] = [
-//             element.querySelector(".swiper"),
-//             element.querySelector(".slider-nav__item_prev"),
-//             element.querySelector(".slider-nav__item_next"),
-//         ];
-
-//         console.log("first")
-
-//         list.push(
-//             new Swiper(slider, {
-//                 effect: "coverflow",
-//                 slidesPerView: 3,
-//                 spaceBetween: 40,
-//                 loop: true,
-//                 speed: 800,
-//                 observer: true,
-//                 watchOverflow: true,
-//                 watchSlidesProgress: true,
-//                 centeredSlides: true,
-//                 initialSlide: 1,
-//                 navigation: { nextEl, prevEl, disabledClass: "disabled" },
-//                 coverflowEffect: {
-//                     rotate: 0,
-//                     stretch: 0,
-//                     depth: 10,
-//                     modifier: 1,
-//                     slideShadows: false,
-//                     scale: 0.85,
-//                 },
-//                 breakpoints: {
-//                     1280: { 
-//                         speed: 800,
-//                         spaceBetween: 0, 
-//                         slidesPerView: 3,
-//                         coverflowEffect: {
-//                             rotate: 0,
-//                             stretch: 80,
-//                             depth: 50,
-//                             scale: 0.5,
-//                         },
-
-//                      }
-
-//                 }
-//             })
-//         );
-//     });
-
-// });
-
-
 /*=============== SWIPER JS ===============*/
 var swiper = new Swiper(".card__content", {
     effect: "coverflow",
@@ -83,13 +24,10 @@ var swiper = new Swiper(".card__content", {
         prevEl: ".slider-nav__item_prev",
     },
     breakpoints: {
-        1980: {
-            speed: 800,
+
+        1920: {
             spaceBetween: 0,
-            slidesPerView: 3,
-
-
-        }
+        },
 
     }
 });
--- js/test.js
+++ js/test.js
@@ -2,32 +2,33 @@ var swiper = new Swiper(".card__content", {
     effect: "coverflow",
     // grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 3,
+    slidesPerView: "auto",
     initialSlide: 2,
     loop: true,
     speed: 500,
     spaceBetween: 64,
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 50,
-        modifier: 1,
-        slideShadows: false,
-        scale: 0.85,
-    },
 
     navigation: {
         nextEl: ".slider-nav__item_next",
         prevEl: ".slider-nav__item_prev",
     },
+
     breakpoints: {
-        1980: {
-            speed: 800,
-            spaceBetween: 0,
+
+        1280: {
             slidesPerView: 3,
-          
-
+            spaceBetween:16,
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 0,
+                depth: 50,
+                modifier: 1,
+                slideShadows: false,
+                scale: 0.85,
+            },
         }
-
     }
+
+
 });
+
