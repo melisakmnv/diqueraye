1dc260fc23203868106455b103e2cb6612f08673
Ekaraja <melisakmnv@gmail.com>
2025-03-24 14:18:37 +0100

    fixed alert

--- js/enigme.js
+++ js/enigme.js
@@ -2,7 +2,8 @@
 const FLIP_BUTTON = document.getElementById('flip-button')
 const FLIP_CARD = document.getElementById('flip-card')
 const SAVE_BUTTON = document.getElementById('save-button')
-
+let hasSaved = false; // save flag
+ 
 
 
 FLIP_BUTTON.addEventListener('click', function () {
@@ -11,8 +12,24 @@ FLIP_BUTTON.addEventListener('click', function () {
 });
 
 
-document.addEventListener("visibilitychange", function () {
-    if (document.hidden) {
-        alert("N'oubliez pas de sauvegarder votre partie avant de");
+// Once this button is click, there is no alert //
+SAVE_BUTTON.addEventListener('click', function () {
+   console.log("saved")
+    hasSaved = true;
+})
+
+
+// BUILT IN ALERT with no cancellation
+
+// document.addEventListener("visibilitychange", function () {
+//     if (document.hidden && !hasSaved) {
+//         alert("N'oubliez pas de sauvegarder votre partie avant de partir !");
+//     }
+// });
+
+window.addEventListener("beforeunload", function (event) {
+    if (!hasSaved) {
+        event.returnValue = "Vous avez des modifications non sauvegardées. Voulez-vous vraiment quitter ?";
+        return "Vous avez des modifications non sauvegardées. Voulez-vous vraiment quitter ?";
     }
 });
