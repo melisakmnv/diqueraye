857161e519ca03f54a7e9778e72b2e28531727a3
Ekaraja <melisakmnv@gmail.com>
2025-03-24 11:05:51 +0100

    added icon blue sky, conditions & term checkbox, alert on save button and select subjects

--- js/enigme.js
+++ js/enigme.js
@@ -1,6 +1,7 @@
 
 const FLIP_BUTTON = document.getElementById('flip-button')
 const FLIP_CARD = document.getElementById('flip-card')
+const SAVE_BUTTON = document.getElementById('save-button')
 
 
 
@@ -8,3 +9,8 @@ FLIP_BUTTON.addEventListener('click', function () {
     // Toggle the "flipped" class on the card to flip it
     FLIP_CARD.classList.toggle('flipped');
 });
+
+
+SAVE_BUTTON.addEventListener('click', function () {
+    alert("Vous pouvez sauvegarder votre partie :)");
+})
